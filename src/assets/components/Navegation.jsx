e8b699129cdc27f36ebcade12efe8e47ff34e00b
ingrid-mo <morales.ingrid.i20@gmail.com>
2023-11-25 14:50:03 -0300

    agregue un badge que avisa cuando hay cosas en el carro

--- src/assets/components/Navegation.jsx
+++ src/assets/components/Navegation.jsx
@@ -1,18 +1,21 @@
+import { useState, useEffect, useContext } from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { FaShoppingCart } from 'react-icons/fa';
 import { NavLink } from 'react-router-dom';
-import { useContext } from 'react';
 import { ContexApi } from '../context/ContextApi';
 import ModalCarrito from './Modal';
-import { useState } from 'react';
+import Badge from 'react-bootstrap/Badge';
+import { IoIosPizza } from "react-icons/io";
+
 const Navegacion = () => {
-  
-  const { precioTotal } = useContext(ContexApi);
+  const { carrito } = useContext(ContexApi);
   const [showModal, setShowModal] = useState(false);
 
-  
+  useEffect(() => {
+    // Puedes hacer algo adicional aquí si necesitas realizar alguna acción cuando cambia el carrito.
+  }, [carrito]);
 
   const handleCartButtonClick = () => {
     setShowModal(true);
@@ -22,35 +25,36 @@ const Navegacion = () => {
     setShowModal(false);
   };
 
-
-  const setActiveClass = ({ isActive }) =>
-    isActive
-      ? 'text-light mt-2 pe-2 text-decoration-none'
-      : 'text-warning mt-2 pe-2 text-decoration-none';
-
-
-  console.log('PRECIOOOOOOOOOOOOOOOOOOOOOOO' +  precioTotal );
+  const determineBadgeColor = () => {
+    return carrito.length > 0 ? 'danger' : 'secondary';
+  };
 
   return (
-    <Navbar className='navbar__section' bg='dark' data-bs-theme='dark'>
-      <Container className='navbar__container'>
-        <Navbar.Brand href='#home' className='logo'>
-          <img src='https://firebasestorage.googleapis.com/v0/b/weather-7f851.appspot.com/o/Group%2060.png?alt=media&token=caf90c15-856f-4d94-a71b-587c6df6f04a' alt='mamamia-logo' />
+    <Navbar className="navbar__section" bg="dark" data-bs-theme="dark">
+      <Container className="navbar__container">
+        <Navbar.Brand href="#home" className="logo">
+          <img
+            src="https://firebasestorage.googleapis.com/v0/b/weather-7f851.appspot.com/o/Group%2060.png?alt=media&token=caf90c15-856f-4d94-a71b-587c6df6f04a"
+            alt="mamamia-logo"
+          />
         </Navbar.Brand>
 
-        <Nav className='me-auto'>
-        <div className='nav__text'>
-          <NavLink to='/' className={setActiveClass}>
-            Home
-          </NavLink>
-          <NavLink  className={setActiveClass} onClick={handleCartButtonClick}>
-            <FaShoppingCart />
-          </NavLink>
-        </div>
-      </Nav>
-      
+        <Nav className="me-auto">
+          <div className="nav__text">
+            <NavLink to="/" className="text-light mt-2 pe-2 text-decoration-none">
+              Home
+            </NavLink>
+            <NavLink className="text-warning mt-2 pe-2 text-decoration-none fs-3" onClick={handleCartButtonClick}>
+              <FaShoppingCart />
+              <div className='badge'>  
+               <Badge bg={determineBadgeColor()}  ><IoIosPizza />
+                </Badge></div>
+           
+            </NavLink>
+          </div>
+        </Nav>
       </Container>
-      <ModalCarrito show={showModal} onHide={handleCloseModal} /* Otros props según tu implementación */ />
+      <ModalCarrito show={showModal} onHide={handleCloseModal} />
     </Navbar>
   );
 };
